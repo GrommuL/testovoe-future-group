2b4311d745559f0918ae69edc3fc2c06b089a29c
Grommul <miketsvg@gmail.com>
2023-10-10 16:47:28 +0600

    update: добавил фильтрацию и сортировку

--- src/components/widgets/header/ui/header.tsx
+++ src/components/widgets/header/ui/header.tsx
@@ -4,6 +4,9 @@ import { SearchIcon } from '@/components/shared/icons/search-icon'
 import { useSearch } from '@/hooks/use-search'
 
 import style from './header.module.scss'
+import { Select } from '@/components/shared/select'
+import { categories } from '@/common/categories'
+import { sortBy } from '@/common/sort-by'
 
 export const Header = () => {
   const { getSearchValue } = useSearch()
@@ -16,7 +19,10 @@ export const Header = () => {
             <Input leftIcon={<SearchIcon />} placeholder='Find a book...' />
             <Button type='submit'>Search</Button>
           </form>
-          <div></div>
+          <div className={style.header_filters}>
+            <Select options={categories} />
+            <Select options={sortBy} />
+          </div>
         </div>
       </div>
     </header>
