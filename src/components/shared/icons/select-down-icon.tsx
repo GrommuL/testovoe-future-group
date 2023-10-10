acc3a7f052fb3bd07e6f8b6f7c6c82aeac3689fc
Grommul <miketsvg@gmail.com>
2023-10-10 16:45:35 +0600

    feat: добавил иконки select-up и select-down

---
+++ src/components/shared/icons/select-down-icon.tsx
@@ -0,0 +1,12 @@
+export const SelectDownIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+    >
+      <path fillRule='evenodd' clipRule='evenodd' d='M7 10L12 15L17 10H7Z' />
+    </svg>
+  )
+}
---
+++ src/components/shared/icons/select-up-icon.tsx
@@ -0,0 +1,12 @@
+export const SelectUpIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+    >
+      <path fillRule='evenodd' clipRule='evenodd' d='M7 14L12 9L17 14L7 14Z' />
+    </svg>
+  )
+}
