400c95280be2716e3803dab3fc75b5441c7eb90f
Grommul <miketsvg@gmail.com>
2023-10-11 12:12:56 +0600

    update: добавил title для хедера

--- src/components/widgets/header/ui/header.tsx
+++ src/components/widgets/header/ui/header.tsx
@@ -2,11 +2,11 @@ import { Button } from '@/components/shared/button'
 import { Input } from '@/components/shared/input'
 import { SearchIcon } from '@/components/shared/icons/search-icon'
 import { useSearch } from '@/hooks/use-search'
-
-import style from './header.module.scss'
 import { Select } from '@/components/shared/select'
 import { categories } from '@/common/categories'
 import { sortBy } from '@/common/sort-by'
+
+import style from './header.module.scss'
 
 export const Header = () => {
   const { getSearchValue } = useSearch()
@@ -15,8 +15,13 @@ export const Header = () => {
     <header className={style.header}>
       <div className='container'>
         <div className={style.header_content}>
+          <h2 className={style.header_title}>Search for books</h2>
           <form className={style.header_search} onSubmit={getSearchValue}>
-            <Input leftIcon={<SearchIcon />} placeholder='Find a book...' />
+            <Input
+              leftIcon={<SearchIcon />}
+              placeholder='Find a book...'
+              type='search'
+            />
             <Button type='submit'>Search</Button>
           </form>
           <div className={style.header_filters}>
