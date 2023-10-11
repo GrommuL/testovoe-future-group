96496f192825c579117da4c87b0c04ef942f7f7b
Grommul <miketsvg@gmail.com>
2023-10-11 12:13:52 +0600

    update: добавил кнопку перезагрузки страницы

--- src/components/widgets/page-error/ui/page-error.tsx
+++ src/components/widgets/page-error/ui/page-error.tsx
@@ -6,15 +6,24 @@ import style from './page-error.module.scss'
 
 export const PageError = () => {
   const navigate = useNavigate()
+
+  const reloadPage = () => {
+    location.reload()
+  }
+
   return (
     <main>
       <section>
         <div className='container'>
           <div className={style.page_error}>
-            <h1 className={style.title}>Something went wrong 🧐</h1>
-            <Button onClick={() => navigate(RoutePath.home)}>
-              Back to Home Page
-            </Button>
+            <h1 className={style.title}>Something went wrong</h1>
+            <div className={style.buttons}>
+              <Button onClick={reloadPage}>Reload Page</Button>
+              or
+              <Button onClick={() => navigate(RoutePath.home)}>
+                Back to Home Page
+              </Button>
+            </div>
           </div>
         </div>
       </section>
